7973cd35f7bc6e65d5687d537d6bbc3b993c701d
Donray Williams <w.donray@yahoo.com>
2017-03-09 12:48:02 -0600

    Almost working

--- GetNeigbor.py
+++ GetNeigbor.py
@@ -1,35 +1,46 @@
-import os
-import random
-import sys
-
 class Node(object):
-	''' A Node '''
+    	''' A Node '''
 	def __init__(self, value, id):
 		self.__id = id
 		self.__value = value
+		
 	def print_info(self):
-		print "Value: ", self.value, "ID: ", self.id
+    	print 'Node: ', self.__id, 'Value: ', self.__value
 
 class Graph(object):
 	''' A Graph '''
-	def __init__(self, nodes):
-		self.__nodes = nodes
+	def __init__(self, pos):
+		self.__pos = pos
+		self.do_work()
+		self.nodes = {}
 
-	def get_node(self, id):
-		''' Get a node by an ID '''
-		if self.nodes[id] in self.nodes:
-			node = self.__nodes[id]
-		return node
+	def do_work(self):				
+		nodes = []
+		for i in range(0, 3):
+			for j in range(0, 3):
+				node = Node([i,j], len(nodes))
+				nodes.append(node)
+		return nodes
 
-def CreateNode():
-	''' Creates a Node that has a ID with it '''
-	nodes = []
-	for i in range(0, 25):
-		node = Node(i*i , i)
-		nodes.append(node)
-	graph = Graph(nodes)
-	gotnode = graph.get_node(10)
-	gotnode.print_info()
+def get_neighbors(N, G):
+	node = G.get_node(N)
+	if node is not None:
+		front = G.get_node(N + 1)
+		behind = G.get_node(N - 1)
+	return [front.print_info(), behind.print_info()]
 
-CreateNode()
-raw_input()
+def get_node(index, g):
+		if g.nodes.has_key(index):
+			return g.nodes[index]
+
+
+def test_nodes():
+	graph = Graph([3, 3])
+	node = get_node(2, graph)
+	node.print_info()
+	neighbors = get_neighbors(node, graph)
+	for nod in neighbors:
+		nod.print_info()
+
+if __name__ == '__main__':
+	test_nodes()
